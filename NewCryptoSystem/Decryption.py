8a11d87421d178af0c745c2e0cf384eb3991934b
Consistent_Milk <mahmudulhasanfh91@gmail.com>
2022-12-30 20:17:38 +0600

    Shared Key System Completed

--- NewCryptoSystem/Decryption.py
+++ NewCryptoSystem/Decryption.py
@@ -1,32 +1,66 @@
 import numpy as np
 import numba as nb
 
+
 class Decrypt:
 
     def __init__(self, shared_key: str, encrypted_points: np.ndarray) -> None:
         self.shared_key = shared_key
         self.encrypted_points = encrypted_points
-        
+
         self._private_parameter_extractor(self.shared_key)
-    
+
     def _private_parameter_extractor(self) -> None:
 
-        # This code may become too complex, think of simpler strategies
+        key_list = list(self.shared_key)
 
-        parity_bits = self.shared_key[::2]
-        pre_parameters = self.shared_key[1::2]
+        digit_bit_indices = []
 
-        parameters = []
-        for parity, param in zip(parity_bits, pre_parameters):
-            
-            if parity == 1:
-                parameters.append(int(param))
+        current_digit_bit_index = 1
+
+        digit_bit_indices.append(current_digit_bit_index)
+
+        current_digit_bit_value = int(key_list[current_digit_bit_index])
+
+        while True:
+
+            print(digit_bit_indices)
+
+            try:
+                current_digit_bit_index = current_digit_bit_index + current_digit_bit_value + 2
+
+                current_digit_bit_value = int(
+                    key_list[current_digit_bit_index])
+
+                digit_bit_indices.append(current_digit_bit_index)
+            except:
+                break
+
+        parity_bit_indices = [x-1 for x in digit_bit_indices]
+
+        parity_bit_values = [int(key_list[x]) for x in parity_bit_indices]
+
+        digit_bit_values = [int(key_list[x]) for x in digit_bit_indices]
+
+        parameter = []
+
+        parity = 0
+
+        for index, skip in zip(digit_bit_indices, digit_bit_values):
+            param_start = index + 1
+            param_end = index + skip + 1
+
+            param = key_list[param_start:param_end]
+            print(param)
+            param = "".join(param)
+            param = int(param)
+
+            if parity_bit_values[parity] == 1:
+                parameter.append(param)
             else:
-                parameters.append(int(param) * (-1))
+                param = param * (-1)
+                parameter.append(param)
 
-        self.h1, self.k1, self.h2, self.k2, self.n1, self.n2 = parameters
+            parity = parity + 1
 
-    
-    
-
-    
+        self.h1, self.k1, self.h2, self.k2, self.n1, self.n2 = parameter
